a7051db35fd3a796c14f98b458f2453d8ec92f17
Yosorable <1354464666@qq.com>
2023-02-18 15:25:12 +0800

    feat: faas add http trigger

--- protoc_gen/faas/req_res.pb.go
+++ protoc_gen/faas/req_res.pb.go
@@ -576,6 +576,108 @@ func (*UpdateFaasTaskByIDReply) Descriptor() ([]byte, []int) {
 	return file_protos_faas_req_res_proto_rawDescGZIP(), []int{9}
 }
 
+type HttpCallTaskRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Path   string  `protobuf:"bytes,1,opt,name=Path,proto3" json:"Path,omitempty"`
+	Params *string `protobuf:"bytes,2,opt,name=params,proto3,oneof" json:"params,omitempty"`
+}
+
+func (x *HttpCallTaskRequest) Reset() {
+	*x = HttpCallTaskRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_protos_faas_req_res_proto_msgTypes[10]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *HttpCallTaskRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*HttpCallTaskRequest) ProtoMessage() {}
+
+func (x *HttpCallTaskRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_protos_faas_req_res_proto_msgTypes[10]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use HttpCallTaskRequest.ProtoReflect.Descriptor instead.
+func (*HttpCallTaskRequest) Descriptor() ([]byte, []int) {
+	return file_protos_faas_req_res_proto_rawDescGZIP(), []int{10}
+}
+
+func (x *HttpCallTaskRequest) GetPath() string {
+	if x != nil {
+		return x.Path
+	}
+	return ""
+}
+
+func (x *HttpCallTaskRequest) GetParams() string {
+	if x != nil && x.Params != nil {
+		return *x.Params
+	}
+	return ""
+}
+
+type HttpCallTaskReply struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Result *string `protobuf:"bytes,1,opt,name=result,proto3,oneof" json:"result,omitempty"`
+}
+
+func (x *HttpCallTaskReply) Reset() {
+	*x = HttpCallTaskReply{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_protos_faas_req_res_proto_msgTypes[11]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *HttpCallTaskReply) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*HttpCallTaskReply) ProtoMessage() {}
+
+func (x *HttpCallTaskReply) ProtoReflect() protoreflect.Message {
+	mi := &file_protos_faas_req_res_proto_msgTypes[11]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use HttpCallTaskReply.ProtoReflect.Descriptor instead.
+func (*HttpCallTaskReply) Descriptor() ([]byte, []int) {
+	return file_protos_faas_req_res_proto_rawDescGZIP(), []int{11}
+}
+
+func (x *HttpCallTaskReply) GetResult() string {
+	if x != nil && x.Result != nil {
+		return *x.Result
+	}
+	return ""
+}
+
 var File_protos_faas_req_res_proto protoreflect.FileDescriptor
 
 var file_protos_faas_req_res_proto_rawDesc = []byte{
@@ -650,6 +752,15 @@ var file_protos_faas_req_res_proto_rawDesc = []byte{
 	0x63, 0x74, 0x69, 0x6f, 0x6e, 0x5f, 0x63, 0x6f, 0x64, 0x65, 0x42, 0x09, 0x0a, 0x07, 0x5f, 0x65,
 	0x6e, 0x61, 0x62, 0x6c, 0x65, 0x22, 0x19, 0x0a, 0x17, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x46,
 	0x61, 0x61, 0x73, 0x54, 0x61, 0x73, 0x6b, 0x42, 0x79, 0x49, 0x44, 0x52, 0x65, 0x70, 0x6c, 0x79,
+	0x22, 0x51, 0x0a, 0x13, 0x48, 0x74, 0x74, 0x70, 0x43, 0x61, 0x6c, 0x6c, 0x54, 0x61, 0x73, 0x6b,
+	0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x12, 0x0a, 0x04, 0x50, 0x61, 0x74, 0x68, 0x18,
+	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x50, 0x61, 0x74, 0x68, 0x12, 0x1b, 0x0a, 0x06, 0x70,
+	0x61, 0x72, 0x61, 0x6d, 0x73, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x48, 0x00, 0x52, 0x06, 0x70,
+	0x61, 0x72, 0x61, 0x6d, 0x73, 0x88, 0x01, 0x01, 0x42, 0x09, 0x0a, 0x07, 0x5f, 0x70, 0x61, 0x72,
+	0x61, 0x6d, 0x73, 0x22, 0x3b, 0x0a, 0x11, 0x48, 0x74, 0x74, 0x70, 0x43, 0x61, 0x6c, 0x6c, 0x54,
+	0x61, 0x73, 0x6b, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x12, 0x1b, 0x0a, 0x06, 0x72, 0x65, 0x73, 0x75,
+	0x6c, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x48, 0x00, 0x52, 0x06, 0x72, 0x65, 0x73, 0x75,
+	0x6c, 0x74, 0x88, 0x01, 0x01, 0x42, 0x09, 0x0a, 0x07, 0x5f, 0x72, 0x65, 0x73, 0x75, 0x6c, 0x74,
 	0x42, 0x30, 0x5a, 0x2e, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x59,
 	0x6f, 0x73, 0x6f, 0x72, 0x61, 0x62, 0x6c, 0x65, 0x2f, 0x6d, 0x73, 0x2d, 0x73, 0x68, 0x61, 0x72,
 	0x65, 0x64, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x63, 0x5f, 0x67, 0x65, 0x6e, 0x2f, 0x66, 0x61,
@@ -668,7 +779,7 @@ func file_protos_faas_req_res_proto_rawDescGZIP() []byte {
 	return file_protos_faas_req_res_proto_rawDescData
 }
 
-var file_protos_faas_req_res_proto_msgTypes = make([]protoimpl.MessageInfo, 10)
+var file_protos_faas_req_res_proto_msgTypes = make([]protoimpl.MessageInfo, 12)
 var file_protos_faas_req_res_proto_goTypes = []interface{}{
 	(*CreateFaasTaskRequest)(nil),     // 0: faas.CreateFaasTaskRequest
 	(*CreateFaasTaskReply)(nil),       // 1: faas.CreateFaasTaskReply
@@ -680,11 +791,13 @@ var file_protos_faas_req_res_proto_goTypes = []interface{}{
 	(*GetFaasTaskByIDReply)(nil),      // 7: faas.GetFaasTaskByIDReply
 	(*UpdateFaasTaskByIDRequest)(nil), // 8: faas.UpdateFaasTaskByIDRequest
 	(*UpdateFaasTaskByIDReply)(nil),   // 9: faas.UpdateFaasTaskByIDReply
-	(*FaasTask)(nil),                  // 10: faas.FaasTask
+	(*HttpCallTaskRequest)(nil),       // 10: faas.HttpCallTaskRequest
+	(*HttpCallTaskReply)(nil),         // 11: faas.HttpCallTaskReply
+	(*FaasTask)(nil),                  // 12: faas.FaasTask
 }
 var file_protos_faas_req_res_proto_depIdxs = []int32{
-	10, // 0: faas.GetFaasTaskByPageReply.faas_tasks:type_name -> faas.FaasTask
-	10, // 1: faas.GetFaasTaskByIDReply.faas_task:type_name -> faas.FaasTask
+	12, // 0: faas.GetFaasTaskByPageReply.faas_tasks:type_name -> faas.FaasTask
+	12, // 1: faas.GetFaasTaskByIDReply.faas_task:type_name -> faas.FaasTask
 	2,  // [2:2] is the sub-list for method output_type
 	2,  // [2:2] is the sub-list for method input_type
 	2,  // [2:2] is the sub-list for extension type_name
@@ -819,19 +932,45 @@ func file_protos_faas_req_res_proto_init() {
 				return nil
 			}
 		}
+		file_protos_faas_req_res_proto_msgTypes[10].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*HttpCallTaskRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_protos_faas_req_res_proto_msgTypes[11].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*HttpCallTaskReply); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	file_protos_faas_req_res_proto_msgTypes[0].OneofWrappers = []interface{}{}
 	file_protos_faas_req_res_proto_msgTypes[2].OneofWrappers = []interface{}{}
 	file_protos_faas_req_res_proto_msgTypes[3].OneofWrappers = []interface{}{}
 	file_protos_faas_req_res_proto_msgTypes[4].OneofWrappers = []interface{}{}
 	file_protos_faas_req_res_proto_msgTypes[8].OneofWrappers = []interface{}{}
+	file_protos_faas_req_res_proto_msgTypes[10].OneofWrappers = []interface{}{}
+	file_protos_faas_req_res_proto_msgTypes[11].OneofWrappers = []interface{}{}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_protos_faas_req_res_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   10,
+			NumMessages:   12,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
--- protoc_gen/faas/service_grpc.pb.go
+++ protoc_gen/faas/service_grpc.pb.go
@@ -29,6 +29,7 @@ type FaasClient interface {
 	GetFaasTaskByID(ctx context.Context, in *GetFaasTaskByIDRequest, opts ...grpc.CallOption) (*GetFaasTaskByIDReply, error)
 	UpdateFaasTaskByID(ctx context.Context, in *UpdateFaasTaskByIDRequest, opts ...grpc.CallOption) (*UpdateFaasTaskByIDReply, error)
 	RunTask(ctx context.Context, in *RunTaskRequest, opts ...grpc.CallOption) (*RunTaskReply, error)
+	HttpCallTask(ctx context.Context, in *HttpCallTaskRequest, opts ...grpc.CallOption) (*HttpCallTaskReply, error)
 }
 
 type faasClient struct {
@@ -93,6 +94,15 @@ func (c *faasClient) RunTask(ctx context.Context, in *RunTaskRequest, opts ...gr
 	return out, nil
 }
 
+func (c *faasClient) HttpCallTask(ctx context.Context, in *HttpCallTaskRequest, opts ...grpc.CallOption) (*HttpCallTaskReply, error) {
+	out := new(HttpCallTaskReply)
+	err := c.cc.Invoke(ctx, "/faas.Faas/HttpCallTask", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // FaasServer is the server API for Faas service.
 // All implementations must embed UnimplementedFaasServer
 // for forward compatibility
@@ -103,6 +113,7 @@ type FaasServer interface {
 	GetFaasTaskByID(context.Context, *GetFaasTaskByIDRequest) (*GetFaasTaskByIDReply, error)
 	UpdateFaasTaskByID(context.Context, *UpdateFaasTaskByIDRequest) (*UpdateFaasTaskByIDReply, error)
 	RunTask(context.Context, *RunTaskRequest) (*RunTaskReply, error)
+	HttpCallTask(context.Context, *HttpCallTaskRequest) (*HttpCallTaskReply, error)
 	mustEmbedUnimplementedFaasServer()
 }
 
@@ -127,6 +138,9 @@ func (UnimplementedFaasServer) UpdateFaasTaskByID(context.Context, *UpdateFaasTa
 }
 func (UnimplementedFaasServer) RunTask(context.Context, *RunTaskRequest) (*RunTaskReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method RunTask not implemented")
+}
+func (UnimplementedFaasServer) HttpCallTask(context.Context, *HttpCallTaskRequest) (*HttpCallTaskReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method HttpCallTask not implemented")
 }
 func (UnimplementedFaasServer) mustEmbedUnimplementedFaasServer() {}
 
@@ -249,6 +263,24 @@ func _Faas_RunTask_Handler(srv interface{}, ctx context.Context, dec func(interf
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Faas_HttpCallTask_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(HttpCallTaskRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(FaasServer).HttpCallTask(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/faas.Faas/HttpCallTask",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(FaasServer).HttpCallTask(ctx, req.(*HttpCallTaskRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Faas_ServiceDesc is the grpc.ServiceDesc for Faas service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -279,6 +311,10 @@ var Faas_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "RunTask",
 			Handler:    _Faas_RunTask_Handler,
+		},
+		{
+			MethodName: "HttpCallTask",
+			Handler:    _Faas_HttpCallTask_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
